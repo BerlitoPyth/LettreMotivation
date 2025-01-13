a727f220bf57f72eb4f6e0bffc177a262fed75a8
BerlitoPyth <berliatadrien@gmail.com>
2025-01-13 12:07:06 +0100

    Update lettre_motivation.py

--- lettre_motivation.py
+++ lettre_motivation.py
@@ -88,7 +88,7 @@ def main():
             except Exception as e:
                 st.info("📸 Photo non disponible")
 
-        st.title("Candidature BUT Science des Données, Berliat Adrien :")
+        st.title("Candidature BUT Science des Données, BERLIAT Adrien")
         st.markdown("---")
 
         # Points clés
