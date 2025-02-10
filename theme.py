2759bda5793b881461c66b1b904afc7cf62328d5
BerlitoPyth <berliatadrien@gmail.com>
2025-02-10 10:03:31 +0100

    Update theme.py

--- theme.py
+++ theme.py
@@ -9,17 +9,18 @@ def toggle_theme():
         st.session_state.dark_mode = not st.session_state.dark_mode
 
     dark_css = """
-        <style>
-        body { background-color: #0e1117; color: white; }
-        .stApp { background-color: #0e1117; }
-        </style>
-    """
+    <style>
+    body, .stApp { background-color: #0e1117; color: white; }
+    h1, h2, h3, h4, h5, h6, p, span, div { color: white !important; }
+    </style>
+"""
 
-    light_css = """
-        <style>
-        body { background-color: white; color: black; }
-        .stApp { background-color: white; }
-        </style>
-    """
+light_css = """
+    <style>
+    body, .stApp { background-color: white; color: black; }
+    h1, h2, h3, h4, h5, h6, p, span, div { color: black !important; }
+    </style>
+"""
+
 
     st.markdown(dark_css if st.session_state.dark_mode else light_css, unsafe_allow_html=True)
