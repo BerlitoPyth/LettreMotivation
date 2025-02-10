0874886c60ee6d687e91152ac7bba23dd6cf0b7c
BerlitoPyth <berliatadrien@gmail.com>
2025-02-10 10:09:42 +0100

    Update theme.py

--- theme.py
+++ theme.py
@@ -13,12 +13,13 @@ def toggle_theme():
     body, .stApp { background-color: #0e1117; color: white; }
     h1, h2, h3, h4, h5, h6, p, span, div { color: white !important; }
     </style>
-"""
+    """
 
-light_css = """
+    light_css = """
     <style>
     body, .stApp { background-color: white; color: black; }
     h1, h2, h3, h4, h5, h6, p, span, div { color: black !important; }
     </style>
-"""
+    """
+    
     st.markdown(dark_css if st.session_state.dark_mode else light_css, unsafe_allow_html=True)
